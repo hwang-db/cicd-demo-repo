d3c7a16d3376fb9b4b4e0b78dc573df69184591e
hao.wang@databricks.com <hao.wang@databricks.com>
2024-06-04 09:22:02 +0000

    first commit
    create schema for dev and prod

---
+++ setup tables.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+USE CATALOG main;
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS hwang_dev;
+CREATE SCHEMA IF NOT EXISTS hwang_prod;
+
+-- COMMAND ----------
+
+
